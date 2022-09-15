0262161c89dd8196a1b11f4cc1ee9923b4b341fc
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-14 21:05:06 -0700

    task 4

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,28 @@ void print_alphabet_x10(void);
 
 int _islower(int c);
 
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c);
+
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0 or -1
+ */
+
+int print_sign(int n);
+
+/**
+ * _abs - gives absolute value
+ * @int: integer
+ *Return: integer or abs value of int
+ */
+
+int _abs(int);
+
 #endif
