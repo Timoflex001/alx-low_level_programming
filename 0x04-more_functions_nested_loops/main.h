6f791aa40750c56f6d5ba783d3cccc7b9c6e4039
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-16 15:03:56 -0700

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -26,4 +26,19 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+/**
+ * print_numbers - prints the numbers
+ *
+ */
+
+void print_numbers(void);
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+
+int _putchar(char c);
+
 #endif
