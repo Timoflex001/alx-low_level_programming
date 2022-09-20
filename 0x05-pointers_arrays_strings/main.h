24c5c994630f41255d46aba622a27c9ee5cdd413
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-19 19:47:15 -0700

    task 2

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -24,4 +24,13 @@ void reset_to_98(int *n);
  */
 
 void swap_int(int *a, int *b);
+
+/**
+ * _strlen - length of string
+ * @s: string
+ *
+ * Return: returns length
+ */
+
+int _strlen(char *s);
 #endif
