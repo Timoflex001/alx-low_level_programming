a77a99089b4fce5b0bb715801e9a022952393d65
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-22 09:01:29 -0700

    task 5

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -56,4 +56,12 @@ int _strcmp(char *s1, char *s2);
  */
 
 void reverse_array(int *a, int n);
+
+/**
+ * string_toupper - makes lower to upper
+ * @s: character to make upper
+ * Return: character value
+ */
+
+char *string_toupper(char *s);
 #endif
