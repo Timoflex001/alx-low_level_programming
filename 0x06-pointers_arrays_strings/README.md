49c87ced3fb552f2716293e8efabe554c48a2687
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-22 07:28:01 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains more pointers and arrays codes
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+
+int _putchar(char c);
+
+/**
+ * _strcat - concats strings
+ * @dest: destination of string
+ * @src: source of string to concat
+ *
+ * Return: returns char value
+ */
+
+char *_strcat(char *dest, char *src);
+
+#endif
