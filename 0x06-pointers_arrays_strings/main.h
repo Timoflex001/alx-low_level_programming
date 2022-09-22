90541db872a0451908eb8e11704ae5b230c5efe2
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-22 10:08:29 -0700

    task 6

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -64,4 +64,12 @@ void reverse_array(int *a, int n);
  */
 
 char *string_toupper(char *s);
+
+/**
+ * cap_string - string to capitalize;
+ * @s: string to capitalize
+ * Return: return the string
+ */
+
+char *cap_string(char *s);
 #endif
