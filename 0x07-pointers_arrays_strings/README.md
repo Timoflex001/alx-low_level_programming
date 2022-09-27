da775ab56fc8c09e9d0f2b03769cc808c3ff6541
Timothy Ayogu <ayogutimothy999@gmail.com>
2022-09-26 19:24:30 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains more pointers and strings codes.
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+int _putchar(char c);
+
+/**
+ * _memset - set memory to a character
+ * @s: value and pointer
+ * @b: value to replace at pointers
+ * @n: n amount of times to replace
+ * Return: returns a pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
